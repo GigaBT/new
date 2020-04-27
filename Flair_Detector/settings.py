ff2c9f00441c08ca384cb05c60b4a6f3d41371b0
GigaBT <64382171+GigaBT@users.noreply.github.com>
2020-04-27 06:00:56 +0530

    Update settings.py

--- Flair_Detector/settings.py
+++ Flair_Detector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't78+vafgjyz-0m5^d_=egu9!xg)%2gs9b)5ds5a)gcxf1rzb(n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['flairdetect.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['flairdetect.herokuapp.com', '127.0.0.1', 'flairodetect.herokuapp.com']
 
 # Application definition
 
